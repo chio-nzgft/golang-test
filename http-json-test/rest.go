d67fdc9225bb78d09df78488d10021b410cd62ea
chio9.chio9 <chio-nzgft@users.noreply.github.com>
2017-06-06 16:18:45 +0800

    Update rest.go

--- http-json-test/rest.go
+++ http-json-test/rest.go
@@ -22,17 +22,19 @@ type Fruits map[string]int
 type Vegetables map[string]int
 // end set Payload struct 
 
-
-// 
+// for get get json to push http
 func serveRest(w http.ResponseWriter, r *http.Request) {
+ // use getJsonResponse() to get json
 	response, err := getJsonResponse()
 	if err != nil {
 		panic(err)
 	}
-
+        
+// push to response
 	fmt.Fprintf(w, string(response))
 }
 
+// main
 func main() {
 	// Http HandleFunc from serveRest func
 	http.HandleFunc("/", serveRest)
@@ -40,7 +42,7 @@ func main() {
 	http.ListenAndServe("localhost:1337", nil)
 }
 
-//Json data info
+//Json data info return []byte, error
 func getJsonResponse() ([]byte, error) {
 	fruits := make(map[string]int)
 	fruits["Apples"] = 25
